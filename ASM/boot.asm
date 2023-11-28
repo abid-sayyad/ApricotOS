e3c9a717335657588fe3c373958ab52616a5eddf
abid-sayyad <sayyad.abid16@gmail.com.com>
2023-11-28 16:15:43 +0530

    Added dummy BIOS parameter block

--- ASM/boot.asm
+++ ASM/boot.asm
@@ -1,9 +1,13 @@
-ORG 0
+ ORG 0
 BITS 16
+_start:
+	jmp short start
+	nop
 
-jmp 0x7c0:start
-
+times 33 db 0
 start:
+	jmp 0x7bf:step2
+step2:
 	cli ;Clear interrupts
 	mov ax, 0x7c0
 	mov ds, ax
@@ -12,6 +16,7 @@ start:
 	mov ss, ax
 	mov sp, 0x7c00
 	sti ;Enables interrupts
+
 	mov si, message
 	call print
 	jmp $
