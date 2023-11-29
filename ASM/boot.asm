c4378137a28d60cd8ed276c4a018c0ffa60fc7af
abid-sayyad <sayyad.abid16@gmail.com.com>
2023-11-29 13:28:31 +0530

    added interrupt 1

--- ASM/boot.asm
+++ ASM/boot.asm
@@ -14,6 +14,14 @@ handle_zero:
 	mov bx, 0x00
 	int 0x10
 	iret
+
+handle_one:
+	mov ah, 0eh
+	mov al, 'V'
+	mov bx, 0x00
+	int 0x10
+	iret
+
 step2:
 	cli ;Clear interrupts
 	mov ax, 0x7c0
@@ -26,8 +34,11 @@ step2:
 
 	mov word[ss:0x00], handle_zero
 	mov word[ss:0x02], 0x7c0
-	
-	int 0
+
+	mov word[ss:0x04], handle_one
+	mov word[ss:0x06], 0x7c0
+
+	int 1
 
 	mov si, message
 	call print
